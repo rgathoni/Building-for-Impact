2750c2714c722c332960523e8d155617835bbf4b
Rose <gathoni@uni.minerva.edu>
2023-10-11 21:17:42 +0800

    Initialized integrated model
    Added the integrated model's frontend interfaces

---
+++ Integrated Model/integrated/src/App.js
@@ -0,0 +1,61 @@
+import React, {useState } from "react";
+import "./App.css";
+import useClipboard from "react-use-clipboard";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import Webcam from "react-webcam";
+
+const App = () => {
+  const [textToCopy, setTextToCopy] = useState("");
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+    successDuration: 1000,
+  });
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  const webcamRef = React.useRef(null);
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  return (
+    <div className="container">
+      <h2>Transcription & Sign Detection</h2>
+      <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+        {transcript}
+      </div>
+
+      <div className="btn-style">
+        <button onClick={setCopied}>
+          {isCopied ? "Copied!" : "Copy to clipboard"}
+        </button>
+        <button onClick={startListening}>Start Listening</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop Listening!</button>
+      </div>
+
+      {/* Webcam component */}
+      <div className="webcam-container">
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          width={640}
+          height={480}
+          screenshotFormat="image/jpeg"
+          
+        />
+      </div>
+      <div className="btn-style">
+        {/* Add functionality to flip Camera */}
+        <button onClick={startListening}>Front Camera</button>
+        <button onClick={SpeechRecognition.stopListening}>Back Camera</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
